b7e4c342bff8369d66f13ed4665b241d42ca3b1a
Jackong <JackongC@gmail.com>
2014-09-15 22:29:38 +0800

    add custom splash

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ cc.game.onStart = function(){
     cc.view.setDesignResolutionSize(320, 480, cc.ResolutionPolicy.SHOW_ALL);
     cc.view.resizeWithBrowserSize(true);
     //load resources
-    cc.LoaderScene.preload(g_resources, function () {
+    LoaderScene.preload(g_resources, function () {
         cc.director.runScene(new MenuScene());
     }, this);
 };
